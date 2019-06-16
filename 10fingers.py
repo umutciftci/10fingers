9e2bf81293ccb5abdcd5179fc7f5e8b31c343e44
Umutc <umutc@umutc.local>
2019-06-16 15:04:54 +0300

    Uploaded python file

---
+++ 10fingers.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver_path = "/home/umutc/Downloads/chromedriver"
+driver = webdriver.Chrome(executable_path=driver_path)
+
+driver.get("https://10fastfingers.com/typing-test/english/")
+
+time.sleep(1)
+#get the word
+for i in range(1,289):
+    word = driver.find_element_by_xpath("//*[@id='row1']/span[%d]" %(i,))
+
+#input box
+    input_box = driver.find_element_by_xpath("//*[@id='inputfield']")
+    input_box.send_keys(word.text + " ")
+
+
+
+
+
+
+
